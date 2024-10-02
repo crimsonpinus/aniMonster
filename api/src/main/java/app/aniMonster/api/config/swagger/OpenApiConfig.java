f66c99ae929dc2a02f7a24f937b3cf4ecd5da6d6
crimsonpinus <crimsonpinus@outlook.com>
2024-10-02 16:15:31 +0900

    result refresh token describe add

--- api/src/main/java/app/aniMonster/api/config/swagger/OpenApiConfig.java
+++ api/src/main/java/app/aniMonster/api/config/swagger/OpenApiConfig.java
@@ -16,7 +16,17 @@ public class OpenApiConfig {
             /open/** -> token 불필요\n
             /api/** -> token 필수\n
             social_id 값 요구하는 api들은 token에서 받아옴 swagger가 오인식 하여 표출되는 경우(아무 문자나 적고 테스트)\n
-            ***개발시에는 바로 호출하여 사용 가능***
+            ***개발시에는 바로 호출하여 사용 가능***\n
+            
+            ***result를 이용한 Token 재발급 ****\n
+                {
+                  "result": {
+                    "result_code": 299,
+                    "result_message": "REFRESH_TOKEN",
+                    "result_description": "jwt 리프레시 토킅"
+                  },
+                  "body": "string"
+                }
             """;
     @Bean
     public OpenAPI OpenAPIConfig() {
